36468133a59af57f17c988f1f8e748dedd430e1c
Firmansyah Yanuar <fyfirman@gmail.com>
2021-02-07 02:36:21 +0700

    create dark toggler

--- src/components/darkToggler/darkToggler.js
+++ src/components/darkToggler/darkToggler.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import Moon from "../../images/moon-sharp.svg"
+import Sun from "../../images/sunny-sharp.svg"
+import "./dark-toggler.scss"
 
 const DarkToggler = () => {
   return <ThemeToggler>
     {({ theme, toggleTheme }) => (
-      <label>
-        <input
-          type="checkbox"
-          onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-          checked={theme === 'dark'}
-        />{' '}
-            Dark mode
-      </label>
+      <button
+        onClick={() => toggleTheme(theme === 'dark' ? 'light' : 'dark')}
+        className="dark-toggler"
+      >
+        {theme !== 'dark' ? <img src={Sun} alt="Light Mode" /> : <img src={Moon} alt="Dark Mode" />}
+      </button>
     )}
   </ThemeToggler>
 }
