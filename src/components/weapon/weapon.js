592f9f18da52d67b20628705db8f0e34f61403cb
Firmansyah Yanuar <fyfirman@gmail.com>
2021-02-07 05:01:21 +0700

    weapon v1

--- src/components/weapon/weapon.js
+++ src/components/weapon/weapon.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './weapon.scss'
 
-const Weapon = ({ name }) => {
+const Weapon = ({ name, color }) => {
   const imageURI = require(`../../images/stack/${name.toLowerCase().split(' ').join('-')}.svg`)
+
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    setTheme(localStorage.getItem('theme'))
+  }, [])
 
   return (
     <div className="weapon">
       <img
         className="weapon-image"
         src={imageURI}
-        alt={name} />
+        alt={name}
+        style={theme === 'dark' ? { outline: `1px solid ${color}` } : {}} />
       <span className="tooltip" data-hover={name}>{name}</span>
     </div>
   )
