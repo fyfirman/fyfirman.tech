e45b6e6ec499a3cba76a09eaa028537187c927bd
Firmansyah Yanuar <fyfirman@gmail.com>
2021-05-13 02:28:09 +0700

    feat: add sender info

--- src/pages/message.js
+++ src/pages/message.js
@@ -2,12 +2,14 @@ import React, { useState } from "react"
 import Layout from "../components/layout"
 import * as styles from "../styles/index.styles"
 import firebase from "gatsby-plugin-firebase"
+import axios from "axios"
 import "../styles/message.scss"
 
 const MessagePage = () => {
   const [name, setName] = useState('')
   const [message, setMessage] = useState('')
   const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(null)
   const [isSuccess, setIsSuccess] = useState(null)
 
   const handleSubmit = (event) => {
@@ -16,14 +18,48 @@ const MessagePage = () => {
       setError({ isError: true, message: "Please fill your message" })
       return;
     }
-    firebase.database().ref('messages/').push({ senderName: name, message })
-      .then(function (snapshot) {
+    setIsLoading(true);
+    axios.get("http://www.geoplugin.net/json.gp").then((res) => {
+      console.log(res.data)
+
+      firebase.database().ref('messages/').push({
+        date: new Date().toLocaleString(),
+        senderName: name,
+        info: {
+          userAgent: navigator.userAgent,
+          platform: navigator.platform,
+          ip: res.data.geoplugin_request,
+          country: res.data.geoplugin_countryName,
+          region: res.data.geoplugin_region,
+          city: res.data.geoplugin_city,
+          geolocation: {
+            latitude: res.data.geoplugin_latitude,
+            longitude: res.data.geoplugin_longitude,
+            accuracy: res.data.geoplugin_locationAccuracyRadius
+          }
+        },
+        message
+      }).then(function (snapshot) {
         console.log(snapshot)
         setIsSuccess(true);
-      }, (e) => {
-        setError({ isError: true, message: e.message })
-        console.error(error);
+        setIsLoading(false);
       });
+    }).catch((e) => {
+      setError({ isError: true, message: e.message })
+      console.error(error);
+      setIsLoading(false);
+    });
+  }
+
+  const getButtonText = () => {
+    if (isSuccess) {
+      return "Message sent! Thank you"
+    }
+    if (isLoading) {
+      return "Please wait"
+    }
+
+    return "SEND"
   }
 
   return (<Layout title="Firmansyah Yanuar | Message">
@@ -78,16 +114,16 @@ const MessagePage = () => {
         <input
           type="submit"
           className="send-button"
-          value={!isSuccess ? "SEND" : "Message sent! Thank you"}
+          value={getButtonText()}
           onClick={handleSubmit}
-          disabled={isSuccess} />
+          disabled={isSuccess || isLoading} />
         <span style={{
           width: '100%',
           color: '#FF0000',
           fontFamily: 'Nunito Sans',
           fontWeight: 500,
           fontSize: '0.875rem',
-          visibility: !error ? "hidden" : "visible"
+          visibility: error && !isSuccess ? "visible" : "hidden"
         }}>{error?.message ?? ''}</span>
       </form>
     </div>
