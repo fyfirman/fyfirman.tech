f950167d37a25a99f3c77b65dab56571102eda8e
Firmansyah Yanuar <fyfirman@gmail.com>
2021-05-13 00:33:51 +0700

    Error message

--- src/pages/message.js
+++ src/pages/message.js
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import * as styles from "../styles/index.styles"
 import "../styles/message.scss"
 
-const MessagePage = () => (
-  <Layout title="Firmansyah Yanuar | Message">
+const MessagePage = () => {
+  const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!message) {
+      setError(true)
+    }
+    console.log(name, message)
+  }
+
+  return (<Layout title="Firmansyah Yanuar | Message">
     <h3 style={styles.title}>Send me a message</h3>
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <div style={{ flex: 1 }}>
@@ -27,7 +39,7 @@ const MessagePage = () => (
           Please speak with <b>human language</b>, not a programming language. 😁
         </p>
       </div>
-      <div style={{ display: 'flex', flex: 1, gap: 16, flexDirection: 'column' }}>
+      <form style={{ display: 'flex', flex: 1, gap: 16, flexDirection: 'column' }}>
         <input
           type="text"
           style={{
@@ -38,9 +50,11 @@ const MessagePage = () => (
             fontSize: '0.875rem',
             padding: 8,
           }}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Name (You can leave it blank)" />
         <textarea
           placeholder="Put your message here"
+          onChange={(e) => setMessage(e.target.value)}
           style={{
             width: '100%',
             height: '60%',
@@ -55,10 +69,18 @@ const MessagePage = () => (
           type="submit"
           className="send-button"
           value="SEND"
-          onclick="submit()" />
-      </div>
+          onClick={handleSubmit} />
+        <span style={{
+          width: '100%',
+          color: '#FF0000',
+          fontFamily: 'Nunito Sans',
+          fontWeight: 500,
+          fontSize: '0.875rem',
+          visibility: !error ? "hidden" : "visible"
+        }}>Please fill your message</span>
+      </form>
     </div>
-  </Layout>
-)
+  </Layout>)
+}
 
 export default MessagePage
